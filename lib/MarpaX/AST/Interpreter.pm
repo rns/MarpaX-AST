2ac4ecc81d2a4d2a5668119cdcffa45190b40e96
rns <ruslan.shvedov@gmail.com>
2015-08-18 17:04:29 +0300

    interpreter options

--- lib/MarpaX/AST/Interpreter.pm
+++ lib/MarpaX/AST/Interpreter.pm
@@ -8,24 +8,46 @@ use Carp;
 
 sub new {
     my ($class, $ast, $opts) = @_;
-    my $interpreter = {};
-    $interpreter->{prefix} = $opts->{prefix} // 'My::Interpreter';
-    $interpreter->{skip} = { map { $_ => 1 } @{ $opts->{skip} // [] } };
-    # bless $ast nodes to qq{$interpreter->{prefix}::$node_id}
 
-    # bless descendants
+    my $interpreter = {};
+
+    $interpreter->{namespace} = $opts->{namespace} // 'My::Interpreter';
+    for my $opt (qw{skip bless}){
+        if (my $node_spec = $opts->{$opt}){
+            if (ref $node_spec eq "ARRAY"){
+                $interpreter->{$opt} = { map { $_ => 1 } @{ $node_spec } };
+            }
+            else{
+                croak "$opt must be a ref to ARRAY, not $node_spec.";
+            }
+        }
+    }
+
+    # bless nodes
     $ast->walk( {
         visit => sub {
             my ($ast) = @_;
-            return if exists $interpreter->{skip}->{$ast->id};
+            my $node_id = $ast->id;
+
+            if (my $bless_nodes = $interpreter->{bless}){
+                return unless exists $bless_nodes->{$node_id};
+            }
+
+            if (my $skip_nodes = $interpreter->{skip}){
+                return if exists $skip_nodes->{$node_id};
+            }
+
+            # only valid package name characters are allowed
+            $node_id =~ tr/ /_/;
+
             if ( ref $ast ) {
-                my $class = $interpreter->{prefix} . '::' . $ast->id;
+                my $class = $interpreter->{namespace} . '::' . $node_id;
                 croak "Package $class doesn't exist." unless defined *{$class . '::'};
                 CORE::bless $_[0], $class;
             }
         }
     } );
-    bless $ast, $interpreter->{prefix} . '::' . $ast->[0];
+    bless $ast, $interpreter->{namespace} . '::' . $ast->[0];
     $interpreter->{ast} = $ast;
     bless $interpreter, $class;
 }
